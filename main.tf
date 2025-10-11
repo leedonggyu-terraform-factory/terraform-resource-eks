751dae834b93c4579f813071a56efb4247013421
leedonggyu <zkfmapf123@naver.com>
2025-10-11 14:56:23 +0900

    wip-7a635b8a

--- main.tf
+++ main.tf
@@ -40,23 +40,6 @@ module "eks" {
   tags                                   = each.value.tags
 }
 
-module "ebs_csi_irsa" {
-  source   = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  for_each = var.items
-
-  role_name             = "${each.key}-ebs-csi-irsa"
-  attach_ebs_csi_policy = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks[each.key].oidc_provider_arn
-      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
-    }
-  }
-
-  tags = each.value.tags
-}
-
 module "blueprints" {
   source  = "aws-ia/eks-blueprints-addons/aws"
   version = "~> 1.0"

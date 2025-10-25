b11c34ee55ce58713dcc4cd80bf4f8a5a4cb5862
leedonggyu <zkfmapf123@naver.com>
2025-10-25 13:47:31 +0900

    wip-0f15f33d

--- 03_hub_gitops_bridge.tf
+++ 03_hub_gitops_bridge.tf
@@ -7,41 +7,41 @@ resource "null_resource" "eks_kubeconfig" {
 }
 
 ////////////////////////////////////////////////////// hub cluster //////////////////////////////////////////////////////
-module "gitops-bridge" {
-  count = var.cluster_attr.info.type == "hub" && var.cluster_attr.info.create ? 1 : 0
+# module "gitops-bridge" {
+#   count = var.cluster_attr.info.type == "hub" && var.cluster_attr.info.create ? 1 : 0
 
-  source = "gitops-bridge-dev/gitops-bridge/helm"
-  // initial gitops-bridge
-  // hub cluster에만 적용
-  create = var.cluster_attr.info.create
+#   source = "gitops-bridge-dev/gitops-bridge/helm"
+#   // initial gitops-bridge
+#   // hub cluster에만 적용
+#   create = var.cluster_attr.info.create
 
-  cluster = {
-    cluster_name = var.cluster_attr.cluster_name
-    environment  = try(var.cluster_attr.environment, "common")
-    metadata = merge({
-      addons_repo_url      = var.gitops_bridge_attr.addons_repo_url
-      addons_repo_basepath = var.gitops_bridge_attr.addons_repo_basepath
-      addons_repo_path     = var.gitops_bridge_attr.addons_repo_path
-      addons_repo_revision = try(var.gitops_bridge_attr.addons_repo_revision, "main")
-      kubernetes_version   = var.cluster_attr.cluster_version
-    }, var.gitops_bridge_metadata),
-    addons = merge(
-      {
-        for addon, bool in var.addons :
-        addon => bool if addon != "external_dns_route53_zone_arns"
-      },
-    )
-  }
+#   cluster = {
+#     cluster_name = var.cluster_attr.cluster_name
+#     environment  = try(var.cluster_attr.environment, "common")
+#     metadata = merge({
+#       addons_repo_url      = var.gitops_bridge_attr.addons_repo_url
+#       addons_repo_basepath = var.gitops_bridge_attr.addons_repo_basepath
+#       addons_repo_path     = var.gitops_bridge_attr.addons_repo_path
+#       addons_repo_revision = try(var.gitops_bridge_attr.addons_repo_revision, "main")
+#       kubernetes_version   = var.cluster_attr.cluster_version
+#     }, var.gitops_bridge_metadata),
+#     addons = merge(
+#       {
+#         for addon, bool in var.addons :
+#         addon => bool if addon != "external_dns_route53_zone_arns"
+#       },
+#     )
+#   }
 
-  apps = {
-    addons = templatefile("${path.module}/apps.yaml", {
-      cluster_name = var.cluster_attr.cluster_name
-      environment  = try(var.cluster_attr.environment, "common")
-    })
-  }
+#   apps = {
+#     addons = templatefile("${path.module}/apps.yaml", {
+#       cluster_name = var.cluster_attr.cluster_name
+#       environment  = try(var.cluster_attr.environment, "common")
+#     })
+#   }
 
-  depends_on = [null_resource.eks_kubeconfig]
-}
+#   depends_on = [null_resource.eks_kubeconfig]
+# }
 
 ###############################################################
 ####### hub cluster에만 구성

528710508cc02d18eed2bbd40321b86d8d5db8e4
leedonggyu <zkfmapf123@naver.com>
2025-10-26 16:18:24 +0900

    wip-888be382

--- 01_eks.tf
+++ 01_eks.tf
@@ -1,26 +1,26 @@
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     command     = "aws"
-#     args        = ["eks", "get-token", "--cluster-name", "${module.eks.cluster_name}", "--region", "ap-northeast-2"]
-#   }
-# }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", "${module.eks.cluster_name}", "--region", "ap-northeast-2"]
+  }
+}
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       command     = "aws"
-#       args        = ["eks", "get-token", "--cluster-name", "${module.eks.cluster_name}", "--region", "ap-northeast-2"]
-#     }
-#   }
-# }
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", "${module.eks.cluster_name}", "--region", "ap-northeast-2"]
+    }
+  }
+}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"

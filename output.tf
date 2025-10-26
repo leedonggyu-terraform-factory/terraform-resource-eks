4a9fcee72de8a121b4c30fcf4221426d5aa2c635
leedonggyu <zkfmapf123@naver.com>
2025-10-26 19:06:52 +0900

    wip-8534a49d

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "eks" {
 output "blueprints" {
   value = module.blueprints
 }
+
+output "cluster_name" {
+  value = var.cluster_attr.cluster_name
+}
+
+output "environment" {
+  value = var.cluster_attr.environment
+}

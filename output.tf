cb597f2abe2583c5f7288f5f1acddfc34c4c44f9
leedonggyu <zkfmapf123@naver.com>
2025-10-26 20:05:43 +0900

    wip-28ac8587

--- output.tf
+++ output.tf
@@ -13,3 +13,11 @@ output "cluster_name" {
 output "environment" {
   value = var.cluster_attr.environment
 }
+
+output "cluster_enpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}

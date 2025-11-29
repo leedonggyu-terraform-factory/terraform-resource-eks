a7ca6e9c8b58e76aead5614b5fc6b3d03257a070
leedonggyu <zkfmapf123@naver.com>
2025-11-29 21:25:37 +0900

    update

---
+++ output.tf
@@ -0,0 +1,23 @@
+output "eks" {
+  value = module.eks
+}
+
+output "blueprints" {
+  value = module.blueprints
+}
+
+output "cluster_name" {
+  value = var.cluster_attr.cluster_name
+}
+
+output "environment" {
+  value = var.cluster_attr.environment
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
--- provider.tf
+++ provider.tf
@@ -8,16 +8,3 @@ provider "kubernetes" {
     args        = ["eks", "get-token", "--cluster-name", "${module.eks.cluster_name}", "--region", "ap-northeast-2"]
   }
 }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       command     = "aws"
-#       args        = ["eks", "get-token", "--cluster-name", "${module.eks.cluster_name}", "--region", "ap-northeast-2"]
-#     }
-#   }
-# }
